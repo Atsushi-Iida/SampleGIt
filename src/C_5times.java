bc64621a6d6130abf75098371952c4eea52b4c67
iida <atsushi.iida.work@gmail.com>
2017-06-28 18:19:03 +0900

    test

---
+++ src/C_5times.java
@@ -0,0 +1,29 @@
+
+public class C<T> {
+	C() {
+	}
+
+	C(T t) {
+	}
+
+	C(C<T> c) {
+	}
+
+	static <U> C<U> m(C<U> c) {
+		return c;
+	}
+
+	public static void main(String[] args) {
+		C<String> c =
+			m(new C<>(
+				m(new C<>(
+					m(new C<>(
+						m(new C<>(
+							m(new C<>(
+							))
+						))
+					))
+				))
+			));
+	}
+}
--- src/Hoge.java
+++ src/Hoge.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Hoge {
 	public static void main(String[] args) {
@@ -10,7 +13,45 @@ public class Hoge {
 		list.add("d");
 
 		list.forEach(System.out::println);
-		list.stream().filter(obj -> "a".equals(obj) || "c".equals(obj)).map(obj -> obj + "Room.").forEach(System.out::println);
+		list.stream().filter(obj -> "a".equals(obj) || "c".equals(obj)).map(obj -> obj + "Room.")
+				.forEach(System.out::println);
+
+		// Supplierは遅延実行されるので、必要になった段階で実施してほしい重い処理を実行するのに便利？
+		Supplier<String> s = () -> "abc";
+		System.out.println(s.get());
+
+		Supplier<String> str = ((Supplier<String>) () -> {
+			System.out.println("str を初期化します！");
+			return "ほげほげ";
+		});
+
+		System.out.println(str.get());
+		System.out.println(str.get());
+		System.out.println(str.get());
+
+		dispTriangle(11);
+
+	}
+
+	private static void dispTriangle(int max) {
+		// IntStream.range(0, max).boxed().forEach(i -> {
+		// IntStream.range(0, i + 1).boxed().limit(5).map(r ->
+		// "*").forEach(System.out::print);
+		// System.out.println();
+		// });
+
+		// for (int count = 0, starSize = 0; count < max; count++) {
+		// starSize = (max / 2 + 1) > count ? starSize + 1 : starSize -1;
+		// IntStream.range(0, starSize).boxed().map(i ->
+		// "*").forEach(System.out::print);
+		// System.out.println();
+		// }
+
+		IntStream.rangeClosed(0, max).map(n -> (max + 1) /2 >= n ? n : max - n + 1).mapToObj(n -> IntStream.range(0, n).mapToObj(m -> "*").collect(Collectors.joining())).forEach(System.out::println);
+
+
+
+
 
 	}
 }
